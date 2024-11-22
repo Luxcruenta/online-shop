1a8b914d2abd6aaa34bec8102564f92f00c506d3
Luxcruenta <166252994+Luxcruenta@users.noreply.github.com>
2024-11-22 23:30:09 +0300

    aaaa1

---
+++ main.js
@@ -0,0 +1,58 @@
+const iPhone = document.querySelector(".iPhone");
+const samsung = document.querySelector(".samsung");
+
+iPhone.addEventListener('click', function(event) {
+    let clickTarget = event.target;
+    let activeButton = iPhone.querySelector('.active');
+    let priceItem = iPhone.querySelector('.price');
+    let currentPrice;
+    let activeButtonColor = iPhone.querySelector('.active-border');
+
+
+if(clickTarget.classList.contains('color-btn') && !clickTarget.classList.contains('active-border')) {
+    clickTarget.classList.add('active-border');
+    activeButtonColor.classList.remove('active-border');
+}
+
+if(clickTarget.classList.contains('btn') && !clickTarget.classList.contains('active')) {
+    clickTarget.classList.add('active');
+    activeButton.classList.remove('active');
+} 
+
+currentPrice = clickTarget.getAttribute('data-size');
+if (currentPrice === 'medium') {
+    priceItem.textContent = 'Цена 80.000 руб.';
+}
+
+if (currentPrice === 'large') {
+    priceItem.textContent = 'Цена 180.000 руб.';
+}
+});
+
+samsung.addEventListener('click', function(event) {
+    let clickTarget = event.target;
+    let activeButton = samsung.querySelector('.active');
+    let priceItem = samsung.querySelector('.price');
+    let currentPrice;
+    let activeButtonColor = samsung.querySelector('.active-border');
+
+
+if(clickTarget.classList.contains('color-btn') && !clickTarget.classList.contains('active-border')) {
+    clickTarget.classList.add('active-border');
+    activeButtonColor.classList.remove('active-border');
+}
+
+if(clickTarget.classList.contains('btn') && !clickTarget.classList.contains('active')) {
+    clickTarget.classList.add('active');
+    activeButton.classList.remove('active');
+} 
+
+currentPrice = clickTarget.getAttribute('data-size');
+if (currentPrice === 'medium') {
+    priceItem.textContent= 'Цена 40.000 руб.';
+}
+
+if (currentPrice === 'large') {
+    priceItem.textContent= 'Цена 70.000 руб.';
+}
+})
